6385fa4288086bbec34a3181c3d9eff927d5a6eb
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-05-19 08:49:59 -0600

    Creating the first trivia online

--- lib/trivia_web/live/trivia_view.ex
+++ lib/trivia_web/live/trivia_view.ex
@@ -1,29 +1,40 @@
 defmodule Trivia.TriviaView do
   use Phoenix.LiveView
 
+  alias Trivia.Game
+  alias Trivia.Player
+  alias Trivia.GameServer
+
   def render(assigns) do
     ~L"""
     <div class="">
+    <%= if @in_game != nil do %>
+      <%= @trivia.status %>
+      <%= else %>
       <div>
-        <div>
-          <button phx-click="add_counter">+</button>
-          <button phx-click="sub_counter">-</button>
-        </div>
-        Status: <%= @counter %>
-      </div>
+        <button phx-click="create_trivia" phx-value="Ruben">Create + Join</button>
+        Number of trivias: <%= @number_of_trivias %>
+    </div>
+      <% end %>
     </div>
     """
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, counter: 0)}
+    nerds = DynamicSupervisor.count_children(Trivia.DynamicSupervisor)
+    socket = assign(socket, counter: 0, number_of_trivias: nerds[:active],
+    in_game: nil, trivia: nil)
+    {:ok, socket}
   end
 
-  def handle_event("add_counter", _value, socket) do
-    {:noreply, assign(socket, :counter, socket.assigns.counter + 1)}
-  end
-
-  def handle_event("sub_counter", _value, socket) do
-    {:noreply, assign(socket, :counter, socket.assigns.counter - 1)}
+  def handle_event("create_trivia", value, socket) do
+    game = Game.new(%{name: "game1", player: Player.new(name: value)})
+    {:ok, game_pid} = Trivia.DynamicSupervisor.start_child(game)
+    nerds = DynamicSupervisor.count_children(Trivia.DynamicSupervisor)
+    socket =  assign(socket, :number_of_trivias, nerds[:active])
+    socket =  assign(socket, :in_game, game_pid)
+    socket =  assign(socket, :trivia, GameServer.game(game_pid))
+    GameServer.start_game(game_pid)
+    {:noreply, socket}
   end
 end
