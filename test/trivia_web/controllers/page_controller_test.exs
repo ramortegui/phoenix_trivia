dc8b3c599e138eadafb79d4f0e1e96dd32f68548
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-05-24 13:45:10 -0600

    Fix controller test.

--- test/trivia_web/controllers/page_controller_test.exs
+++ test/trivia_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TriviaWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "https://github.com/ramortegui/phoenix_trivia"
   end
 end
