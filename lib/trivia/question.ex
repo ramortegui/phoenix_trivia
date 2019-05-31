fcc0f8e98cfccd51707462dd3998493a4e43e1a6
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-05-30 21:09:47 -0600

    Fix format

--- lib/trivia/question.ex
+++ lib/trivia/question.ex
@@ -2,7 +2,7 @@ defmodule Trivia.Question do
   @moduledoc """
   Define question structure and validations.
   """
-  defstruct text: "", options: [], answer: ''
+  defstruct text: "", options: [], answer: ""
 
   @doc """
   Returns a `Trivia.Question` Struct with shuffled options, the answer is included as part of the options.
@@ -15,6 +15,9 @@ defmodule Trivia.Question do
   @doc """
   Check the answer of a question
   """
-  def valid_answer?(%__MODULE__{answer: answer}, guess) when answer == guess, do: true
+  def valid_answer?(%__MODULE__{answer: answer}, guess)
+      when answer == guess,
+      do: true
+
   def valid_answer?(_question, _guess), do: false
 end
