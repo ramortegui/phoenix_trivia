068418b29020a2b88223304ab3ffb3224d25b051
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-05-30 18:03:08 -0600

    Update format.

--- lib/trivia_web/controllers/page_controller.ex
+++ lib/trivia_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule TriviaWeb.PageController do
   use TriviaWeb, :controller
+
   def index(conn, _params) do
     render(conn, "index.html")
     #   LiveView.Controller.live_render(conn, TriviaWeb.Live.TriviaView, session: %{})
