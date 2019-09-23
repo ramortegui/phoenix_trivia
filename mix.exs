eb262ffc8c44591b2ed88bf034789eb9adc51cfd
Ruben Amortegui <ruben.amortegui@gmail.com>
2019-09-23 09:28:57 -0600

    Bumb version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Trivia.MixProject do
   def project do
     [
       app: :trivia,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.8.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
